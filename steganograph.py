77de42b513001555f33f8b366a08b152c9970552
Nage-Sh <darklordryuk@gmail.com>
2025-07-12 09:47:54 +0530

    Added the loading home page

--- steganograph.py
+++ steganograph.py
@@ -17,6 +17,21 @@ class SteganographyApp:
 
         # Load Home Page
         self.load_home_page()
+    
+    def load_home_page(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+        self.home_label = tk.Label(self.root, text="Welcome to Text-in-Image Steganography", font=("Arial", 16))
+        self.home_label.pack(pady=20)
+
+        # Go to Encoder page button
+        self.encoder_btn = tk.Button(self.root, text="Go to Encoder", command=self.load_encoder_page)
+        self.encoder_btn.pack(pady=10)
+
+        # Go to Decoder page button
+        self.decoder_btn = tk.Button(self.root, text="Go to Decoder", command=self.load_decoder_page)
+        self.decoder_btn.pack(pady=10)
 
 if __name__ == '__main__':
     root = TkinterDnD.Tk()
